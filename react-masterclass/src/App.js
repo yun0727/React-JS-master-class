cfd202b85b6f740bf7b9c0da7584f78f67eea2ff
yun <austru00@gmail.com>
2023-05-12 02:40:30 +0900

    2.4 Animations and Pseudo Selectors

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,19 +1,48 @@
-import styled from "styled-components"
-const Father = styled.div`
+import styled,{keyframes} from "styled-components"
+
+const Wrapper = styled.div`
   display: flex;
 `
-const Input = styled.input.attrs({required:true})`
-  background-color:tomato
-`
+const rotationAnimation=keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50% {
+    border-radius:100px;
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 48px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
+
 function App(){
   return(
-    <Father as="header">
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🤩</span>
+      </Box>
+    </Wrapper>
   );
 }
 export default App
