bec3d8854b98c376d8f99dec9d13e82625a8315b
yun <austru00@gmail.com>
2023-05-12 02:24:49 +0900

    2.2 Adapting and Extending

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -2,25 +2,19 @@ import styled from "styled-components"
 const Father = styled.div`
   display: flex;
 `
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+const Circle = styled(Box)`
+border-radius:50px;
 `
-const Text = styled.span`
-color: white;`
 function App(){
   return(
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo></BoxTwo>
+      <Box bgColor="teal"/ >
+      <Circle bgColor="tomato"/ >
     </Father>
   );
 }
