685ec11ab7b962a75c92c5409dea5f5606374d2a
yun <austru00@gmail.com>
2023-05-15 17:10:11 +0900

    3.4 State

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -5,7 +5,7 @@ function App(){
   return(
     <div>
       <Circle borderColor="yellow" bgColor="teal" />
-      <Circle text="im here" bgColor="tomato" />
+      <Circle bgColor="tomato" />
     </div>
   );
 }
