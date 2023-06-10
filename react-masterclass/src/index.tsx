0c9b4c9fba8bb7095bc30afa6277f11ffe19177e
yun <austru00@gmail.com>
2023-06-10 15:46:57 +0900

    4.2 CreateBrowserRouter

--- react-masterclass/src/index.tsx
+++ react-masterclass/src/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from './theme';
+import { RouterProvider } from "react-router-dom";
+import router from "./Router";
 
 
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-    <App/>
-    </ThemeProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
