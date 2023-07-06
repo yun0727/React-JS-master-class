e19b3ea73fd7572aa2f50c849974c433d1a445a8
yun <austru00@gmail.com>
2023-07-07 04:31:18 +0900

    5.10 React Query 2

--- react-crypto-tracker/src/routes/Coin.tsx
+++ react-crypto-tracker/src/routes/Coin.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
+
+import {useQuery} from "react-query";
 import { Switch, Route, useLocation, useParams, useRouteMatch } from "react-router";
 import styled from "styled-components";
+import { fetchCoinInfo, fetchCoinTickers } from "../api";
 import Chart from "./Chart";
 import Price from "./Price";
 import { Link } from "react-router-dom";
@@ -9,25 +11,21 @@ const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
-
 const Loader = styled.span`
   text-align: center;
   display: block;
 `;
-
 const Container = styled.div`
   padding: 0px 20px;
   max-width: 480px;
   margin: 0 auto;
 `;
-
 const Header = styled.header`
   height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
-
 const Overview = styled.div`
   display: flex;
   justify-content: space-between;
@@ -76,7 +74,6 @@ interface RouterParams{
 interface RouteState {
   name: string;
 }
-
 interface InfoData{
   id: string;
   name: string;
@@ -97,7 +94,6 @@ interface InfoData{
   first_data_at: string;
   last_data_at: string;
 }
-
 interface PriceData {
   id: string;
   name: string;
@@ -135,29 +131,23 @@ interface PriceData {
 function Coin(){
   const {coinId} = useParams<RouterParams>();
   const { state } = useLocation<RouteState>();
-  const [loading, setLoading] = useState(true);
-  const [info, setInfo] = useState<InfoData>();
-  const [priceInfo, setPriceInfo]=useState<PriceData>();
   const priceMatch=useRouteMatch("/:coinId/price");
   const chartMatch=useRouteMatch("/:coinId/chart");
-  useEffect(()=>{
-    (async ()=>{
-      const infoData = await(
-        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-        ).json();
-      const priceData = await(
-        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
-      ).json();
-      setInfo(infoData);
-      setPriceInfo(priceData);
-      setLoading(false);
-    })();
-  },[coinId])
+  const {isLoading: infoLoading, data:infoData} =useQuery<InfoData>(
+    ["info",coinId],
+    ()=> fetchCoinInfo(coinId)
+  );
+  const {isLoading: tickersLoading, data:tickersData} =useQuery<PriceData>(
+    ["tickers",coinId],
+    ()=> fetchCoinTickers(coinId)
+  );
+  
+  const loading = infoLoading || tickersLoading;
   return (
     <Container>
       <Header>
         <Title>
-          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
         </Title>
       </Header>
       {loading ? (
@@ -167,26 +157,26 @@ function Coin(){
           <Overview>
             <OverviewItem>
               <span>Rank:</span>
-              <span>{info?.rank}</span>
+              <span>{infoData?.rank}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Symbol:</span>
-              <span>${info?.symbol}</span>
+              <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Open Source:</span>
-              <span>{info?.open_source ? "Yes" : "No"}</span>
+              <span>{infoData?.open_source ? "Yes" : "No"}</span>
             </OverviewItem>
           </Overview>
-          <Description>{info?.description}</Description>
+          <Description>{infoData?.description}</Description>
           <Overview>
             <OverviewItem>
               <span>Total Supply:</span>
-              <span>{priceInfo?.total_supply}</span>
+              <span>{tickersData?.total_supply}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Max Supply:</span>
-              <span>{priceInfo?.max_supply}</span>
+              <span>{tickersData?.max_supply}</span>
             </OverviewItem>
           </Overview>
           <Tabs>
--- react-crypto-tracker/src/routes/Coins.tsx
+++ react-crypto-tracker/src/routes/Coins.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { fetchCoins } from "./api";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
   padding: 0px 20px;
