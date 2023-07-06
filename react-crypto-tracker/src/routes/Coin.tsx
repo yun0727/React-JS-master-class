bd8d1cf64196dd600c56eaf395f81722a7b09ae9
yun <austru00@gmail.com>
2023-07-07 03:49:16 +0900

    5.8 Nested Routes 2

--- react-crypto-tracker/src/routes/Coin.tsx
+++ react-crypto-tracker/src/routes/Coin.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router";
+import { Switch, Route, useLocation, useParams, useRouteMatch } from "react-router";
 import styled from "styled-components";
+import Chart from "./Chart";
+import Price from "./Price";
+import { Link } from "react-router-dom";
 
 const Title = styled.h1`
   font-size: 48px;
@@ -24,6 +27,48 @@ const Header = styled.header`
   justify-content: center;
   align-items: center;
 `;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+const Tabs =  styled.div`
+  display:grid;
+  grid-template-columns:repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`
+const Tab = styled.span<{isActive:boolean}>`
+  text-align:center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight:400;
+  background-color: rgba(0,0,0,0.5);
+  padding:7px 0px;
+  border-radius: 10px;
+  color: ${(props)=> props.isActive ? props.theme.accentColor:props.theme.textColor};
+  a{
+    display:block;
+  }
+`
+
 interface RouterParams{
   coinId: string;
 }
@@ -92,6 +137,8 @@ function Coin(){
   const { state } = useLocation<RouteState>();
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo]=useState<PriceData>();
+  const priceMatch=useRouteMatch("/:coinId/price");
+  const chartMatch=useRouteMatch("/:coinId/chart");
   useEffect(()=>{
     (async ()=>{
       const infoData = await(
@@ -102,14 +149,65 @@ function Coin(){
       ).json();
       setInfo(infoData);
       setPriceInfo(priceData);
+      setLoading(false);
     })();
-  },[])
+  },[coinId])
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loading..."}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Supply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+
+
+          <Switch>
+            <Route path={`/:coinId/price`}>
+              <Price />
+            </Route>
+            <Route path={`/:coinId/chart`}>
+              <Chart />
+            </Route>
+          </Switch>
+        </>
+      )}
     </Container>
   );
 }
