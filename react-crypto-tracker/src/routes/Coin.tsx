fddbe6ae139c7cfab870a21312f28fca1503cdc7
yun <austru00@gmail.com>
2023-07-07 04:05:35 +0900

    5.9 React Query 1

--- react-crypto-tracker/src/routes/Coin.tsx
+++ react-crypto-tracker/src/routes/Coin.tsx
@@ -39,6 +39,7 @@ const OverviewItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 33%;
   span:first-child {
     font-size: 10px;
     font-weight: 400;
@@ -61,10 +62,10 @@ const Tab = styled.span<{isActive:boolean}>`
   font-size: 12px;
   font-weight:400;
   background-color: rgba(0,0,0,0.5);
-  padding:7px 0px;
   border-radius: 10px;
   color: ${(props)=> props.isActive ? props.theme.accentColor:props.theme.textColor};
   a{
+    padding: 7px 0px;
     display:block;
   }
 `
@@ -132,9 +133,9 @@ interface PriceData {
 }
 
 function Coin(){
-  const [loading, setLoading] = useState(true);
   const {coinId} = useParams<RouterParams>();
   const { state } = useLocation<RouteState>();
+  const [loading, setLoading] = useState(true);
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo]=useState<PriceData>();
   const priceMatch=useRouteMatch("/:coinId/price");
