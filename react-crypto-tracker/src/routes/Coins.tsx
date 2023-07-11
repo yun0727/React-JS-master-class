9e920894fd8c15b53b7ef9ecd398fcf0bfebe8f6
yun <austru00@gmail.com>
2023-07-12 00:28:13 +0900

    6.0 Dark Mode 1

--- react-crypto-tracker/src/routes/Coins.tsx
+++ react-crypto-tracker/src/routes/Coins.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -8,18 +9,22 @@ const Container = styled.div`
   max-width: 480px;
   margin: 0 auto;
 `;
+
 const Header = styled.header`
   height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
+
 const CoinsList = styled.ul``;
+
 const Coin = styled.li`
-  background-color: white;
-  color: ${(props) => props.theme.bgColor};
+  background-color: ${(props) => props.theme.cardBgColor};
+  color: ${(props) => props.theme.textColor};
   border-radius: 15px;
   margin-bottom: 10px;
+  border: 1px solid white;
   a {
     display: flex;
     align-items: center;
@@ -32,14 +37,17 @@ const Coin = styled.li`
     }
   }
 `;
+
 const Title=styled.h1`
   font-size:48px;
   color:${(props)=>props.theme.accentColor};
 `
+
 const Loader=styled.span`
   text-align: center;
   display: block;
 `
+
 const Img =styled.img`
   width: 35px;
   height:35px;
@@ -54,13 +62,15 @@ interface ICoin{
   is_new: boolean,
   is_active: boolean,
   type: string,
-
 }
 
 function Coins(){
   const {isLoading, data} = useQuery<ICoin[]>("allCoins",fetchCoins);
   return (
     <Container>
+      <Helmet>
+        <Title>코인</Title>
+      </Helmet>
       <Header>
         <Title>코인</Title>
       </Header>
@@ -70,11 +80,12 @@ function Coins(){
         <CoinsList>
           {data?.slice(0,100).map((coin) => (
             <Coin key={coin.id}>
-              <Link to={{
-                pathname: `/${coin.id}`,
-                state:{name:coin.name},
-
-                }}>
+              <Link 
+                to={{
+                  pathname: `/${coin.id}`,
+                  state:{name:coin.name},
+                }}
+              >
                 <Img
                   src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
                 />
--- react-crypto-tracker/src/styled.d.ts
+++ react-crypto-tracker/src/styled.d.ts
@@ -8,5 +8,6 @@ declare module 'styled-components' {
     textColor: string;
     bgColor: string;
     accentColor: string;
+    cardBgColor: string;
   }
 }
