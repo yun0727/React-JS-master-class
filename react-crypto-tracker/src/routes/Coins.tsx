0a58ab48539f17c274d87d4242926c1274391e8d
yun <austru00@gmail.com>
2023-07-12 17:41:45 +0900

    6.3 Introduction to Recoil 2

--- react-crypto-tracker/src/routes/Coins.tsx
+++ react-crypto-tracker/src/routes/Coins.tsx
@@ -3,6 +3,8 @@ import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { fetchCoins } from "../api";
+import { useSetRecoilState } from "recoil";
+import { isDarkAtom } from "../atoms";
 const Container = styled.div`
   padding: 0px 20px;
   max-width: 480px;
@@ -59,7 +61,9 @@ interface ICoin {
 interface ICoinsProps {
 }
 
-function Coins({  }: ICoinsProps) {
+function Coins() {
+  const setDarkAtom = useSetRecoilState(isDarkAtom)
+  const toggleDarkAtom = () => setDarkAtom(prev => !prev)
   const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
   return (
     <Container>
@@ -68,6 +72,7 @@ function Coins({  }: ICoinsProps) {
       </Helmet>
       <Header>
         <Title>코인</Title>
+        <button onClick={toggleDarkAtom}>Toggle Mode</button>
       </Header>
       {isLoading ? (
         <Loader>Loading...</Loader>
