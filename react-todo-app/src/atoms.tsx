d1acdf07dc5023a1213e5e8112d2d5044f2e448d
yun <austru00@gmail.com>
2023-07-13 03:24:20 +0900

    6.16 Selectors 1

--- react-todo-app/src/atoms.tsx
+++ react-todo-app/src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   text: string;
@@ -10,3 +10,15 @@ export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
 });
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({get})=> {
+    const toDos = get(toDoState);
+    return[
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE")
+    ]
+  }
+})
