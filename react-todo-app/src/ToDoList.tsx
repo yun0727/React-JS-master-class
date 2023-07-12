ef0357fb7c58263f12e55db914738ea0d1fb9ba1
yun <austru00@gmail.com>
2023-07-13 01:43:07 +0900

    6.9 Custom Validation

--- react-todo-app/src/ToDoList.tsx
+++ react-todo-app/src/ToDoList.tsx
@@ -8,21 +8,30 @@ interface IForm{
   username: string;
   password: string;
   password1: string;
+  extraError?: string;
 }
 
 function ToDoList() {
     const { 
       register, 
       handleSubmit, 
-      formState:{errors}, 
+      formState:{errors},
+      setError, 
     } = useForm<IForm>({
       defaultValues:{
         email:"@naver.com",
       },
     });
-    const onValid = (data:any) => {
-      console.log(data);
+    const onValid = (data:IForm) => {
+      if (data.password !== data.password1){
+        setError(
+          "password1",
+          {message: "Password are not the same"},
+          {shouldFocus: true}
+        );
+      }
     };
+    console.log(errors);
     return (
       <div>
         <form 
@@ -40,7 +49,13 @@ function ToDoList() {
           />
           <span>{errors?.email?.message}</span>
           <input 
-            {...register("firstName", {required:"write here"})}             
+            {...register("firstName", {
+              required:"write here",
+              validate:{
+                noNico : (value) => value.includes("nico") ? "no nicos allowed":true,
+                noNick : (value) => value.includes("nick") ? "no nick allowed":true,
+              }
+            })}             
             placeholder="First Name" 
           />
           <span>{errors?.firstName?.message}</span>
@@ -71,6 +86,7 @@ function ToDoList() {
           />
           <span>{errors?.password1?.message}</span>
           <button>Add</button>
+          <span>{errors?.extraError?.message}</span>
         </form>
       </div>
     );
